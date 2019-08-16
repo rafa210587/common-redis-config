e1f54a52c865384dd814918ac3fb140d233d5eb3
Rafael Meira Goncalves <rafael.goncalves@trustpay.com.br>
2019-08-16 16:52:52 -0300

    modify with @import

--- src/main/java/comum/datasource/config/DataSourceConfig.java
+++ src/main/java/comum/datasource/config/DataSourceConfig.java
@@ -3,12 +3,10 @@ package comum.datasource.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -22,7 +20,7 @@ import comum.jdbc.sql.AuditSQLServerDataSource;
  * @author rafael.goncalves
  */
 @Configuration
-@ConditionalOnClass
+@ComponentScan
 public class DataSourceConfig {
 
 	@Value("${spring.application.name}")
